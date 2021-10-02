7d066a80fe71273db6a53377d27b741925c8274f
hsmak <husain.alkhamis@gmail.com>
2021-10-02 09:39:17 -0500

    committing changes...

---
+++ bootable-producers-consumers/src/test/java/org/hsmak/kafkastarter/KafkaStarterApplicationTests.java
@@ -0,0 +1,17 @@
+package org.hsmak.kafkastarter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KafkaStarterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
